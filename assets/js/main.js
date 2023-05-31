048c3c6548cb1f179b708c19d79515cf6f8235c2
Aleksandre Gabunia <97845038+agabunia@users.noreply.github.com>
2023-06-01 01:31:13 +0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const contactMe = document.getElementById("contact-me")
 const contactBtn = document.getElementById("get-in-touch")
 const returnToDefault = document.getElementById("main")
+const footerHover = document.getElementById("footer")
 
 contactBtn.addEventListener("click", function(){
     contactMe.classList.remove("contact-me")
@@ -9,6 +10,18 @@ contactBtn.addEventListener("click", function(){
 })
 
 returnToDefault.addEventListener("click", function(){
+    contactMe.classList.remove("contact-me-click")
+    contactMe.classList.add("contact-me")
+    contactBtn.style.display = "block"
+})
+
+footerHover.addEventListener("mouseover", function(){
+    contactMe.classList.remove("contact-me")
+    contactMe.classList.add("contact-me-click")
+    contactBtn.style.display = "none"
+})
+
+returnToDefault.addEventListener("mouseover", function(){
     contactMe.classList.remove("contact-me-click")
     contactMe.classList.add("contact-me")
     contactBtn.style.display = "block"
